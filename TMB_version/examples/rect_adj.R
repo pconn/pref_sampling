a8e15cabbd03811bbeccc2d688dece59733f3564
James-Thorson <JamesT.esq@gmail.com>
2015-09-30 13:28:35 -0700

    adding my initial unbiased-demonstration code

---
+++ TMB_version/examples/rect_adj.R
@@ -0,0 +1,75 @@
+
+#' Produce an RW1 adjacency matrix for a rectangular grid for use with areal spatial models (queens move)
+#' @param x number of cells on horizontal side of grid
+#' @param y number of cells on vertical side of grid
+#' @param byrow If TRUE, cell indices are filled along rows (default is FALSE)
+#' @return adjacency matrix
+#' @export
+#' @keywords adjacency
+#' @author Paul Conn \email{paul.conn@@noaa.gov}
+rect_adj <- function(x,y,byrow=FALSE){
+  Ind=matrix(c(1:(x*y)),y,x,byrow=byrow)
+  if(byrow==TRUE)Ind=t(Ind)
+  n.row=nrow(Ind)
+  n.col=ncol(Ind)
+  Adj=matrix(0,x*y,x*y)
+  for(i in 1:n.row){
+    for(j in 1:n.col){
+      if(i==1 & j==1){
+        Adj[Ind[i,j],Ind[i,j]+1]=1
+        Adj[Ind[i,j],Ind[i,j]+n.row]=1
+        Adj[Ind[i,j],Ind[i,j]+n.row+1]=1
+      }
+      if(i==1 & j>1 & j<n.col){
+        Adj[Ind[i,j],Ind[i,j]+1]=1
+        Adj[Ind[i,j],Ind[i,j]+n.row]=1
+        Adj[Ind[i,j],Ind[i,j]-n.row]=1
+        Adj[Ind[i,j],Ind[i,j]+n.row+1]=1
+        Adj[Ind[i,j],Ind[i,j]-n.row+1]=1
+      }
+      if(i==1 & j==n.col){
+        Adj[Ind[i,j],Ind[i,j]+1]=1
+        Adj[Ind[i,j],Ind[i,j]-n.row]=1
+        Adj[Ind[i,j],Ind[i,j]-n.row+1]=1
+      }
+      if(i>1 & i<n.row & j==1){
+        Adj[Ind[i,j],Ind[i,j]+1]=1
+        Adj[Ind[i,j],Ind[i,j]+n.row]=1
+        Adj[Ind[i,j],Ind[i,j]-1]=1
+        Adj[Ind[i,j],Ind[i,j]+n.row-1]=1
+        Adj[Ind[i,j],Ind[i,j]+n.row+1]=1
+      }
+      if(i>1 & i<n.row & j>1 & j<n.col){
+        cur.nums=c(Ind[i,j]-n.row-1,Ind[i,j]-n.row,Ind[i,j]-n.row+1,Ind[i,j]-1,Ind[i,j]+1,Ind[i,j]+n.row-1,Ind[i,j]+n.row,Ind[i,j]+n.row+1)
+        Adj[Ind[i,j],cur.nums]=1
+      }
+      if(i>1 & i<n.row & j==n.col){
+        Adj[Ind[i,j],Ind[i,j]+1]=1
+        Adj[Ind[i,j],Ind[i,j]-n.row]=1
+        Adj[Ind[i,j],Ind[i,j]-1]=1
+        Adj[Ind[i,j],Ind[i,j]-n.row-1]=1
+        Adj[Ind[i,j],Ind[i,j]-n.row+1]=1
+
+      }
+      if(i==n.row & j==1){
+        Adj[Ind[i,j],Ind[i,j]+n.row]=1
+        Adj[Ind[i,j],Ind[i,j]-1]=1
+        Adj[Ind[i,j],Ind[i,j]+n.row-1]=1
+      }
+      if(i==n.row & j>1 & j<n.col){
+        Adj[Ind[i,j],Ind[i,j]+n.row]=1
+        Adj[Ind[i,j],Ind[i,j]-1]=1
+        Adj[Ind[i,j],Ind[i,j]-n.row]=1
+        Adj[Ind[i,j],Ind[i,j]+n.row-1]=1
+        Adj[Ind[i,j],Ind[i,j]-n.row-1]=1
+      }
+      if(i==n.row & j==n.col){
+        Adj[Ind[i,j],Ind[i,j]-1]=1
+        Adj[Ind[i,j],Ind[i,j]-n.row]=1
+        Adj[Ind[i,j],Ind[i,j]-n.row-1]=1
+      }
+    }
+  }
+  if(byrow==TRUE)Adj=t(Adj)
+  return(Adj)
+}
